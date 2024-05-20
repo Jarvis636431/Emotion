124c5dbdfe44132551439b7c159183a076091f67
mengda636431 <112926380+mengda636431@users.noreply.github.com>
2024-05-20 14:55:41 +0800

    [fea]完善了新建提醒页面

--- lib/PendingPages/PendingPage.dart
+++ lib/PendingPages/PendingPage.dart
@@ -7,69 +7,71 @@ class PendingPage extends StatefulWidget {
 }
 
 class _PendingPageState extends State<PendingPage> {
-  late TabController tabController;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    tabController = TabController(length: 3, vsync: ScrollableState());
-  }
-  @override
-  void dispose() {
-    tabController.dispose();
-    super.dispose();
-  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        title: Text('事件记录'),
-        bottom: TabBar(
-          tabs: [
-            Tab(
-              child: Row(
-                children: [
-                  Icon(Icons.access_time),
-                  Text('事件记录'),
-                ],
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            title: Text('事件记录'),
+            bottom: TabBar(
+              isScrollable: true,
+              tabs: [
+                Tab(
+                  child: Row(
+                    children: [
+                      Icon(Icons.access_time),
+                      Text('事件记录'),
+                    ],
+                  ),
+                ),
+                Tab(
+                  child: Row(
+                    children: [
+                      Icon(Icons.check),
+                      Text('每日总结'),
+                    ],
+                  ),
+                ),
+                Tab(
+                  child: Row(
+                    children: [
+                      Icon(Icons.done_all),
+                      Text('提醒事项'),
+                    ],
+                  )
+                ),
+              ],
+            )),
+        body: Center(
+          child: TabBarView(children: [
+            Container(
+              color: Colors.red,
+              child: Center(
+                child: Text('待处理'),
               ),
             ),
-            Tab(
-              text: '已处理',
+            Container(
+              color: Colors.green,
+              child: Center(
+                child: Text('已处理'),
+              ),
             ),
-            Tab(
-              text: '已完成',
+            Container(
+              color: Colors.blue,
+              child: Center(
+                child: Text('已完成'),
+
+              ),
             ),
-          ],
-        )
-      ),
-      body: Center(
-        child: PageView(children:[
-          Container(
-            color: Colors.red,
-            child: Center(
-              child: Text('待处理'),
-            ),
-          ),
-          Container(
-            color: Colors.green,
-            child: Center(
-              child: Text('已处理'),
-            ),
-          ),
-          Container(
-            color: Colors.blue,
-            child: Center(
-              child: Text('已完成'),
-            ),
-          ),
-        ]),
+          ]),
+        ),
       ),
     );
   }
