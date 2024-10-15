07a3d908ef3238a81089179c4151cd04b7104d83
Jarvis <mengda636431@outlook.com>
2024-10-16 00:09:55 +0800

    [fea]实现了沉浸式底部导航栏布局，并且修复了底部像素溢出的问题//为什么使用listview后仍会溢出呢？

--- lib/screens/user_pages/user_page.dart
+++ lib/screens/user_pages/user_page.dart
@@ -12,105 +12,124 @@ class UserPage extends StatefulWidget {
 }
 
 class _UserPageState extends State<UserPage> {
+
+  //转换十六进制演的方法
+  Color yellow = const Color(0xffFFF5B6);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.settings),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SettingsPage()),
-                  //MeterialPageRoute只是路由手段，push才是方法
-                );
-              },
-            ),
-          ],
-        ),
-        body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.h, vertical: 10.w),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Image.asset(
-                    'assets/images/female.png',
-                    width: 120.h,
-                  ),
-                  Column(children: [
-                    Text(
-                      '菠萝小姐',
-                      style: TextStyle(fontSize: 23.sp),
-                    ),
-                    Text(
-                      '本小姐还没有想到个性签名',
-                      style: TextStyle(fontSize: 10.sp),
-                    ),
-                  ]),
-                ],
-              ),
-              SizedBox(
-                height: 20.h,
-              ),
-              Text('每日一言收藏'),
-              Container(
-                width: 600.h,
-                height: 100.w,
-                decoration: BoxDecoration(
-                  color: Colors.yellow[200],
-                  border: Border.all(color: Colors.transparent),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child:Text('每日一言收藏'),
-              ),
-              Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text('测试')),
-              Container(
-                height: 200.h,
-                child: ListView(
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    Container(
-                      width: 300.h,
-                      decoration: BoxDecoration(
-                        color: Colors.yellowAccent,
-                        border: Border.all(color: Colors.transparent),
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Image.asset('assets/images/testpic1.png'),
-                          Text('测一测你的MBTI人格吧'),
-                        ],
-                      ),
-                    ),
-                    SizedBox(width: 20.h),
-                    Container(
-                      width: 300.h,
-                      decoration: BoxDecoration(
-                        color: Colors.yellowAccent,
-                        border: Border.all(color: Colors.transparent),
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Image.asset('assets/images/testpic2.png'),
-                          Text('测试'),
-                        ],
-                      ),
-                    ),                ],
-                ),
-              ),
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.white, Color(0xffFFF5B6)],
+          //直接用yellow会报错？为什么
 
+          //这样也可以直接使用十六进制的颜色
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      //设置全局背景不仅要设置设置Container的颜色，还要设置scaffold和appbar的背景同时为透明，因为默认是白色
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.settings),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const SettingsPage()),
+                    //MeterialPageRoute只是路由手段，push才是方法
+                  );
+                },
+              ),
             ],
           ),
-        ));
+          body: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.h, vertical: 10.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Image.asset(
+                      'assets/images/female.png',
+                      width: 120.h,
+                    ),
+                    Column(children: [
+                      Text(
+                        '菠萝小姐',
+                        style: TextStyle(fontSize: 23.sp),
+                      ),
+                      Text(
+                        '本小姐还没有想到个性签名',
+                        style: TextStyle(fontSize: 10.sp),
+                      ),
+                    ]),
+                  ],
+                ),
+                SizedBox(
+                  height: 20.h,
+                ),
+                Text('每日一言收藏'),
+                Container(
+                  width: 600.h,
+                  height: 100.w,
+                  decoration: BoxDecoration(
+                    color: Colors.yellow[200],
+                    border: Border.all(color: Colors.transparent),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child:Text('每日一言收藏'),
+                ),
+                Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text('测试')),
+                Container(
+                  height: 200.h,
+                  child: ListView(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      Container(
+                        width: 300.h,
+                        decoration: BoxDecoration(
+                          color: Colors.yellowAccent,
+                          border: Border.all(color: Colors.transparent),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Image.asset('assets/images/testpic1.png'),
+                            Text('测一测你的MBTI人格吧'),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 20.h),
+                      Container(
+                        width: 300.h,
+                        decoration: BoxDecoration(
+                          color: Colors.yellowAccent,
+                          border: Border.all(color: Colors.transparent),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Image.asset('assets/images/testpic2.png'),
+                            Text('测试'),
+                          ],
+                        ),
+                      ),                ],
+                  ),
+                ),
+
+              ],
+            ),
+          )),
+    );
   }
 }
