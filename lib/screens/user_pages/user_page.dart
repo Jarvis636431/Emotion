b7d9088ec1a6542d52de6bab2810c9df9dbd84ae
Jarvis <mengda636431@outlook.com>
2024-10-17 22:32:42 +0800

    [fea]个人页面补充了字体等布局，已经完成

--- lib/screens/user_pages/user_page.dart
+++ lib/screens/user_pages/user_page.dart
@@ -14,9 +14,6 @@ class UserPage extends StatefulWidget {
 }
 
 class _UserPageState extends State<UserPage> {
-
-  //转换十六进制演的方法
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -29,24 +26,39 @@ class _UserPageState extends State<UserPage> {
       ),
       //设置全局背景不仅要设置设置Container的颜色，还要设置scaffold和appbar的背景同时为透明，因为默认是白色
       child: Scaffold(
-        backgroundColor: Colors.transparent,
+          backgroundColor: Colors.transparent,
           appBar: AppBar(
             backgroundColor: Colors.transparent,
             actions: [
-              IconButton(
-                icon: const Icon(Icons.settings),
-                onPressed: () {
+              InkWell(
+                onTap: () {
                   Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const SettingsPage()),
-                    //MeterialPageRoute只是路由手段，push才是方法
-                  );
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const SettingsPage()));
                 },
-              ),
+                child: Padding(
+                  padding: EdgeInsets.only(right: 20.h),
+                  child: Column(
+                    children: [
+                      Image.asset(
+                        'assets/images/settings.png',
+                        width: 30.h,
+                      ),
+                      ///TODO:需要这行文字么，加上之后布局有些奇怪
+                      Text(
+                        '设置',
+                        style: TextStyle(
+                            fontSize: 13.sp,
+                            color: ColorUtils.text_brown),
+                      ),
+                    ],
+                  ),
+                ),)
             ],
           ),
           body: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.h, vertical: 10.w),
+            padding: EdgeInsets.symmetric(horizontal: 20.h),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -56,73 +68,125 @@ class _UserPageState extends State<UserPage> {
                       'assets/images/female.png',
                       width: 120.h,
                     ),
-                    Column(children: [
-                      Text(
-                        '菠萝小姐',
-                        style: TextStyle(fontSize: 23.sp),
-                      ),
-                      Text(
-                        '本小姐还没有想到个性签名',
-                        style: TextStyle(fontSize: 10.sp,color: ColorUtils.text_yellow)
-                      ),
-                    ]),
+                    SizedBox(
+                      width: 20.h,
+                    ),
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '菠萝小姐',
+                            style: TextStyle(
+                                fontSize: 30.sp,
+                                fontFamily: 'LanSong',
+                                color: ColorUtils.text_brown),
+                          ),
+                          Text('本小姐还没有想到个性签名',
+                              style: TextStyle(
+                                  fontSize: 15.sp,
+                                  color: ColorUtils.text_yellow)),
+                        ]),
                   ],
                 ),
                 SizedBox(
-                  height: 20.h,
+                  height: 20.w,
                 ),
-                Text('每日一言收藏'),
+                Text('每日一言收藏',
+                    style: TextStyle(
+                        fontSize: 20.sp,
+                        fontFamily: 'LanSong',
+                        color: ColorUtils.text_brown)),
+                SizedBox(
+                  height: 15.w,
+                ),
                 Container(
-                  width: 600.h,
-                  height: 100.w,
+                  width: 400.h,
+                  height: 184.w,
                   decoration: BoxDecoration(
                     color: ColorUtils.daily_sentence_bg,
-                    border: Border.all(color: Colors.transparent),
                     borderRadius: BorderRadius.circular(20),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: ColorUtils.shadow,
+                        offset: Offset(0, 4),
+                        blurRadius: 4,
+                      ),
+                    ],
                   ),
-                  child:Text('每日一言收藏'),
+                  child: Center(
+                    child: Text(
+                      '每日一言',
+                      style: TextStyle(
+                          fontSize: 20.sp,
+                          color: ColorUtils.text_brown),
+                    )
+                  ),
                 ),
-                Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text('测试')),
-                Container(
+                SizedBox(
+                  height: 15.w,
+                ),
+                Text('测试',
+                    style: TextStyle(
+                        fontSize: 20.sp,
+                        fontFamily: 'LanSong',
+                        color: ColorUtils.text_brown)),
+                SizedBox(
+                  height: 15.w,
+                ),
+                ///TODO：解决子组件的高度跟随父组件的原因并解决，现在存在阴影溢出
+                SizedBox(
                   height: 200.h,
                   child: ListView(
-                    shrinkWrap: true,
                     scrollDirection: Axis.horizontal,
                     children: [
                       Container(
                         width: 300.h,
+                        height: 185.w,
                         decoration: BoxDecoration(
                           color: ColorUtils.card_bg,
                           borderRadius: BorderRadius.circular(20),
+                          boxShadow: const [
+                            BoxShadow(
+                              color: ColorUtils.shadow,
+                              offset: Offset(0, 4),
+                              blurRadius: 4,
+                            ),
+                          ],
                         ),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
                             Image.asset('assets/images/testpic1.png'),
-                            Text('测一测你的MBTI人格吧'),
+                            Text('测一测你的MBTI人格吧',style: TextStyle(fontSize: 20.sp,color: ColorUtils.text_brown)),
                           ],
                         ),
                       ),
                       SizedBox(width: 20.h),
                       Container(
                         width: 300.h,
+                        height: 185.w,
                         decoration: BoxDecoration(
                           color: ColorUtils.card_bg,
                           borderRadius: BorderRadius.circular(20),
+                          boxShadow: const [
+                            BoxShadow(
+                              color: ColorUtils.shadow,
+                              offset: Offset(0, 4),
+                              blurRadius: 4,
+                            ),
+                          ],
                         ),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
                             Image.asset('assets/images/testpic2.png'),
-                            Text('测试'),
+                            Text('测试',style: TextStyle(fontSize: 20.sp,color: ColorUtils.text_brown)),
                           ],
                         ),
-                      ),                ],
+                      ),
+                    ],
                   ),
                 ),
-
               ],
             ),
           )),
