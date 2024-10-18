bb78088166967382d75620d3cdf183814dda95a6
Jarvis <mengda636431@outlook.com>
2024-10-18 21:15:15 +0800

    [fea]实现了冥想页面的功能和结构设计

--- lib/screens/funtions_pages/functions_page.dart
+++ lib/screens/funtions_pages/functions_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:emotion/screens/funtions_pages/fill_color_page.dart';
+import 'package:emotion/screens/funtions_pages/think_page.dart';
 
 import 'package:emotion/utils/ColorUtils.dart';
 
@@ -45,7 +46,7 @@ class _FunctionsPageState extends State<FunctionsPage> {
                 //这样的自定义元素太多就不方便封装
             InkWell(
               onTap: () {
-                Navigator.push(context,MaterialPageRoute(builder: (context) => FillColorPage()));
+                Navigator.push(context,MaterialPageRoute(builder: (context) => const FillColorPage()));
               },
               child: Container(
                 width: 400.h,
@@ -150,104 +151,109 @@ class _FunctionsPageState extends State<FunctionsPage> {
             SizedBox(
               height: 20.w,
             ),
-            Container(
-              width: 400.h,
-              height: 127.w,
-              decoration: BoxDecoration(
-                color: ColorUtils.func_card_bg,
-                borderRadius: BorderRadius.circular(20.r),
-                boxShadow: const [
-                  BoxShadow(
-                    color: ColorUtils.shadow,
-                    offset: Offset(0, 4),
-                    blurRadius: 4,
-                  ),
-                ],
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.h, vertical: 15.w),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          '白噪音冥想',
-                          style: TextStyle(
-                              fontFamily: 'LanSong',
-                              fontSize: 21.sp,
-                              color: ColorUtils.text_brown),
-                        ),
-
-                        ///设置divider
-                        Divider(
-                          color: Colors.green,
-                          height: 10.w,
-                          thickness: 100.h,
-                        ),
-                        Text(
-                          '“闭目凝神，于喧嚣中寻一隅静谧”',
-                          style: TextStyle(fontSize: 13.sp),
-                        ),
-                        SizedBox(
-                          height: 10.w,
-                        ),
-                        Row(
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                color: ColorUtils.info_card_bg,
-                                borderRadius: BorderRadius.circular(6.r),
-                              ),
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: 5.h, vertical: 3.w),
-                                child: Text('#为您推荐'),
-                              ),
-                            ),
-                            SizedBox(
-                              width: 5.h,
-                            ),
-                            Container(
-                              decoration: BoxDecoration(
-                                color: ColorUtils.info_card_bg,
-                                borderRadius: BorderRadius.circular(6.r),
-                              ),
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: 5.h, vertical: 3.w),
-                                child: Text('音乐疗愈'),
-                              ),
-                            ),
-                          ],
-                        )
-                      ],
+            InkWell(
+              onTap: () {
+                Navigator.push(context,MaterialPageRoute(builder: (context) => ThinkPage()));
+              },
+              child: Container(
+                width: 400.h,
+                height: 127.w,
+                decoration: BoxDecoration(
+                  color: ColorUtils.func_card_bg,
+                  borderRadius: BorderRadius.circular(20.r),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: ColorUtils.shadow,
+                      offset: Offset(0, 4),
+                      blurRadius: 4,
                     ),
-                    SizedBox(
-                      width: 10.h,
-                    ),
-                    Container(
-                      width: 79.h,
-                      height: 79.w,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20.r),
-                        gradient: const LinearGradient(
-                          colors: [
-                            ColorUtils.grad_light_green,
-                            ColorUtils.grad_green
-                          ],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                        ),
-                        image: const DecorationImage(
-                          image: AssetImage(
-                              'assets/images/function/emptythink.png'),
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                    )
                   ],
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.h, vertical: 15.w),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '白噪音冥想',
+                            style: TextStyle(
+                                fontFamily: 'LanSong',
+                                fontSize: 21.sp,
+                                color: ColorUtils.text_brown),
+                          ),
+
+                          ///设置divider
+                          Divider(
+                            color: Colors.green,
+                            height: 10.w,
+                            thickness: 100.h,
+                          ),
+                          Text(
+                            '“闭目凝神，于喧嚣中寻一隅静谧”',
+                            style: TextStyle(fontSize: 13.sp),
+                          ),
+                          SizedBox(
+                            height: 10.w,
+                          ),
+                          Row(
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: ColorUtils.info_card_bg,
+                                  borderRadius: BorderRadius.circular(6.r),
+                                ),
+                                child: Padding(
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 5.h, vertical: 3.w),
+                                  child: Text('#为您推荐'),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 5.h,
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: ColorUtils.info_card_bg,
+                                  borderRadius: BorderRadius.circular(6.r),
+                                ),
+                                child: Padding(
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 5.h, vertical: 3.w),
+                                  child: Text('音乐疗愈'),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        width: 10.h,
+                      ),
+                      Container(
+                        width: 79.h,
+                        height: 79.w,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.r),
+                          gradient: const LinearGradient(
+                            colors: [
+                              ColorUtils.grad_light_green,
+                              ColorUtils.grad_green
+                            ],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                          ),
+                          image: const DecorationImage(
+                            image: AssetImage(
+                                'assets/images/function/emptythink.png'),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
---
+++ lib/screens/funtions_pages/think_page.dart
@@ -0,0 +1,267 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+import 'package:emotion/utils/ColorUtils.dart';
+
+class ThinkPage extends StatefulWidget {
+  @override
+  _ThinkPageState createState() => _ThinkPageState();
+}
+
+class _ThinkPageState extends State<ThinkPage> {
+  ///这个居然能用，我靠！神秘，我半年前的代码水平
+
+  Timer? _timer;
+  int _start = 0;
+  bool _isRunning = false;
+
+  ///开始计时
+  void _startTimer() {
+    if (_isRunning) return;
+    setState(() {
+      _isRunning = true;
+    });
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        _start++;
+      });
+    });
+  }
+
+  ///暂停计时
+  void _pauseTimer() {
+    if (!_isRunning) return;
+    setState(() {
+      _isRunning = false;
+    });
+    _timer?.cancel();
+  }
+
+  ///重置计时
+  void _resetTimer() {
+    setState(() {
+      _isRunning = false;
+      _start = 0;
+    });
+    _timer?.cancel();
+  }
+
+  ///格式化时间
+  String _formatTime(int seconds) {
+    final int minutes = seconds ~/ 60;
+    final int remainingSeconds = seconds % 60;
+    return '$minutes:${remainingSeconds.toString().padLeft(2, '0')}';
+  }
+
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  final List<String> _songs = [
+    'assets/songs/meditation.mp3',
+    'assets/songs/meditation2.mp3',
+    'assets/songs/meditation3.mp3',
+  ];
+
+  int _currentSongIndex = 0;
+  bool _isPlaying = false;
+
+  ///播放歌曲/暂停歌曲
+  void _playPause() {
+    if (_isPlaying) {
+      _audioPlayer.pause();
+    } else {
+      _audioPlayer.play(_songs[_currentSongIndex] as Source);
+    }
+    setState(() {
+      _isPlaying = !_isPlaying;
+    });
+  }
+
+  ///下一首
+  void _next() {
+    if (_currentSongIndex < _songs.length - 1) {
+      _currentSongIndex++;
+    } else {
+      _currentSongIndex = 0;
+    }
+    _audioPlayer.play(_songs[_currentSongIndex] as Source);
+    setState(() {
+      _isPlaying = true;
+    });
+  }
+
+  ///上一首
+  void _previous() {
+    if (_currentSongIndex > 0) {
+      _currentSongIndex--;
+    } else {
+      _currentSongIndex = _songs.length - 1;
+    }
+    _audioPlayer.play(_songs[_currentSongIndex] as Source);
+    setState(() {
+      _isPlaying = true;
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [ColorUtils.bg_white, ColorUtils.bg_yellow],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              color: ColorUtils.text_brown,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          title: Text(
+            '冥想',
+            style: TextStyle(
+                fontSize: 24.sp,
+                fontFamily: 'LanSong',
+                color: ColorUtils.text_brown),
+          ),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.refresh, color: ColorUtils.text_brown),
+              onPressed: () {
+                _resetTimer();
+
+                ///重置时间
+              },
+            ),
+          ],
+        ),
+        body: Column(children: [
+          // Stack(
+          //   ///圆形指示器
+          //   children: [
+          //     Container(
+          //       width: 200.w,
+          //       height: 200.h,
+          //       decoration: BoxDecoration(
+          //         shape: BoxShape.circle,
+          //         border: Border.all(
+          //           color: Colors.black,
+          //           width: 2,
+          //         ),
+          //       ),
+          //     ),
+          //     Positioned.fill(
+          //       child: CircularProgressIndicator(
+          //         value: _start / 60,
+          //         valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+          //         backgroundColor: Colors.grey[300],
+          //         strokeWidth: 10,
+          //       ),
+          //     ),
+          //   ],
+          // ),
+          Image(image:AssetImage('assets/images/mbti_character/enf.png'),width: 250.w),
+          Text(
+            //格式换之后的时间
+            _formatTime(_start),
+            style: TextStyle(
+              fontSize: 45.sp,
+              fontFamily: 'LanSong',
+              color: ColorUtils.text_brown,
+            ),
+          ),
+          SizedBox(height: 20.w),
+          Text('正在播放\n音乐${_currentSongIndex + 1}',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20.sp,
+                fontFamily: 'LanSong',
+                color: ColorUtils.text_brown,
+              )),
+
+          ///TODO：实现动态变化
+          ///buttonbar的用法
+          SizedBox(height: 20.w),
+          Row(
+            //要调整位置，实际是要调整row里面元素的位置，而不是row本身的位置，因为row是无限的
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              InkWell(
+                onTap: () {
+                  _previous();
+                },
+                //使用矢量画法
+                child: Transform.rotate(
+                  angle: 3.14,
+                  child: Icon(
+                    Icons.double_arrow_rounded,
+                    color: ColorUtils.text_brown,
+                    size: 40.sp,
+                    textDirection: TextDirection.rtl,
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  _playPause();
+                },
+                icon: Icon(_isPlaying ? Icons.pause : Icons.play_arrow,
+                    color: ColorUtils.text_brown, size: 40.sp),
+              ),
+              InkWell(
+                onTap: () {
+                  _next();
+                },
+                child: Icon(
+                  Icons.double_arrow_rounded,
+                  color: ColorUtils.text_brown,
+                  size: 40.sp,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20.w),
+          InkWell(
+            onTap: () {
+              _isRunning ? _pauseTimer() : _startTimer();
+            },
+            child: Container(
+              width: 300.w,
+              height: 70.h,
+              decoration: BoxDecoration(
+                color: ColorUtils.bg_white,
+                borderRadius: BorderRadius.circular(35.r),
+              ),
+              alignment: Alignment.center,
+              child: Text(
+                _isRunning ? '暂停冥想' : '开始冥想',
+                style: TextStyle(
+                  fontSize: 32.sp,
+                  color: ColorUtils.text_brown,
+                  fontFamily: 'LanSong',
+                ),
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
