23ad192a79e104205f81fe63d3d214eda1c821ab
Jarvis <mengda636431@outlook.com>
2024-10-19 02:19:04 +0800

    [fea]性格影响因素和打卡界面结构基本完成

--- lib/screens/mood_pages/check_mood_page.dart
+++ lib/screens/mood_pages/check_mood_page.dart
@@ -1,3 +1,4 @@
+import 'package:emotion/utils/ColorUtils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -6,48 +7,59 @@ class MoodCheckPage extends StatefulWidget {
   _MoodCheckPageState createState() => _MoodCheckPageState();
 }
 
-class _MoodCheckPageState extends State<MoodCheckPage>{
+class _MoodCheckPageState extends State<MoodCheckPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: (){
-            Navigator.pop(context);
-          },
-        ),
-        title: const Text('打卡成功！'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.star,color: Colors.yellow,),
-            onPressed: (){
-
+        appBar: AppBar(
+          leading: IconButton(
+            icon:
+                const Icon(Icons.arrow_back_ios, color: ColorUtils.text_brown),
+            onPressed: () {
+              Navigator.pop(context);
             },
-          ),],
-      ),
-      body: Stack(
-        children: [
-          Column(
-            children: [
-              Row(
-                children: [
-                  Text('四月\n22日\n星期四'),
-                  Image(image: AssetImage('assets/images/happy.png')),
-                  Text('心情\n一般')
-                ]
-              ),
-              Divider(
-                height: 10,
-                thickness: 1,
-                color: Colors.grey,
-              ),
-              Text(''),
-              Image(image: AssetImage('assets/images/happy.png')),
-            ],
           ),
-        ],
-      )
-    );
+          title: Text('打卡成功！',
+              style: TextStyle(
+                  fontSize: 24.sp,
+                  fontFamily: "LanSong",
+                  color: ColorUtils.text_brown)),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              icon: const Icon(
+                Icons.star,
+                color: Colors.yellow,
+              ),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: Stack(
+          children: [
+            //并非没有重叠，只是column太大了
+            Container(
+              width: 400.h,
+              height: 600.w,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 400.w,
+                    width: 400.h,
+                    child: Row(children: [
+                      Text('四月\n22日\n星期四'),
+                      Image.asset('assets/images/mbti_emoji/enf/enf1.png',width: 60.h,),
+                      Text('心情\n一般')
+                    ]),
+                  ),
+                  Text(''),
+                ],
+              ),
+            ),
+            Image.asset('assets/images/card.png')//卡片有透明边框
+          ],
+        ));
   }
 }
+///TODOL打卡成功之后改怎么办你？
--- lib/screens/mood_pages/mood_infactor_page.dart
+++ lib/screens/mood_pages/mood_infactor_page.dart
@@ -28,50 +28,64 @@ class MoodInfactorPage extends StatelessWidget {
           ),
           title: Text('是什么影响了你的情绪？',style: TextStyle(fontSize: 32.sp,fontFamily: 'LanSong',color: ColorUtils.text_brown),),
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              color: Colors.yellow,
-              width: 400.h,
-              height: 400.w,
-              child: GridView(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                ),
-                ///自定义组件导入图片+文字
-                children: const [
-                  Text('工作'),
-                  Text('学习'),
-                  Text('生活'),
-                  Text('人际关系'),
-                  Text('健康'),
-                  Text('其他'),
-                ],
-              ),
-            ),
-            Expanded(child: SizedBox()),
-            InkWell(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => MoodCheckPage()));
-              },
-              child: Container(
-                  width: 200.h,
-                  height: 60.w,
-                  decoration: BoxDecoration(
-                    color: ColorUtils.bg_white,
-                    borderRadius: BorderRadius.circular(20.r),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.h,vertical: 10.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 50.w),
+              Container(
+                width: 400.h,
+                height: 400.w,//设置一个超出范围的高，使之能完全容纳，否则加上禁止滚动后会出现溢出问题
+                child: GridView.builder( ///布局问题
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 10.h,
+                    mainAxisSpacing: 10.w,
+                    childAspectRatio: 0.9,
                   ),
-                  alignment: Alignment.center,
-                  child:Text('是这样的',style: TextStyle(fontFamily: 'LanSong',fontSize: 24.sp,color: ColorUtils.text_brown),)),//修改字体
-            ),
-            SizedBox(height: 80.w),
-          ],
+                  physics: const NeverScrollableScrollPhysics(),//禁用滚动的方法
+                  itemCount: 9,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        Container(
+                            width: 100.h,
+                            height: 100.w,
+                            decoration: BoxDecoration(
+                              color: ColorUtils.bg_white,
+                              borderRadius: BorderRadius.circular(20.r),
+                            ),
+                            alignment: Alignment.center,
+                            child: Text('图片',style: TextStyle(fontFamily: 'LanSong',fontSize: 24.sp,color: ColorUtils.text_brown),),//修改字体
+                          ),
+                        Text('文字',style: TextStyle(fontFamily: 'LanSong',fontSize: 14.sp,color: ColorUtils.text_brown),),//修改字体
+                      ],
+                    );
+                  },
+                ),
+              ),
+              Expanded(child: SizedBox()),
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => MoodCheckPage()));
+                },
+                child: Container(
+                    width: 200.h,
+                    height: 60.w,
+                    decoration: BoxDecoration(
+                      color: ColorUtils.bg_white,
+                      borderRadius: BorderRadius.circular(20.r),
+                    ),
+                    alignment: Alignment.center,
+                    child:Text('是这样的',style: TextStyle(fontFamily: 'LanSong',fontSize: 24.sp,color: ColorUtils.text_brown),)),//修改字体
+              ),
+              SizedBox(height: 80.w),
+            ],
+          ),
         ),
       ),
     );
